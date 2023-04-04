b0705614d9fb26bdba5c3d3c8f3c921f73e46bfe
davurov <113560218+davurov@users.noreply.github.com>
2023-04-05 02:53:24 +0500

    initial comment

---
+++ Movement/Movement/GameScene.swift
@@ -0,0 +1,98 @@
+//
+//  GameScene.swift
+//  Movement
+//
+//  Created by Abduraxmon on 05/04/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var spinnyNode : SKShapeNode?
+    var player = SKShapeNode(circleOfRadius: 16)
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+        
+        player.strokeColor = .red
+        player.fillColor = .red
+        player.physicsBody = SKPhysicsBody(circleOfRadius: 16)
+        player.physicsBody?.affectedByGravity = false
+        player.position = .init(x: 0, y: 0)
+        addChild(player)
+        
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.01
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.removeFromParent()]))
+        }
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+         
+        let movementSpeed = 200.0
+        
+        let x = pos.x - player.position.x
+        let y = pos.y - player.position.y
+        
+        let distance = sqrt(x * x + y * y)
+        
+        player.run(SKAction.move(to: pos, duration: Double(distance) / movementSpeed))
+        
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
